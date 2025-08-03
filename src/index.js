048685a32446e4426a0155d81fc345c4dba3ba6f
Patrick Samuel <10101460+patricksamuel@users.noreply.github.com>
2025-08-03 20:34:45 +0200

    add and test task delete function.

--- src/index.js
+++ src/index.js
@@ -17,10 +17,10 @@ class Task {
         }
     taskInfo(){
         if (this.doneStatus) {
-            console.log("You have to do: "+this.taskDesc+" by "+this.dueDate+" And you did it on "+this.doneDate);
+            console.log("Task Id:" +this.taskId+". You have to do: "+this.taskDesc+" by "+this.dueDate+" And you did it on "+this.doneDate);
             }
         else {
-            console.log("You have to do: "+this.taskDesc+" by "+this.dueDate+" not done yet");
+            console.log("Task Id:" +this.taskId+". You have to do: "+this.taskDesc+" by "+this.dueDate+" not done yet");
             }
         }
     }
@@ -32,7 +32,27 @@ function addTask(taskDesc, dueDate, doneStatus) {
     allTasks.push(newTask);
 }
 
+function deleteTask(taskId) {
+    const index = allTasks.findIndex((task) => task.taskId === taskId)
+    if (index !== -1){
+        allTasks.splice(index,1);
+        console.log("element number"+taskId+" is deleted");
+    }
+    else {
+        console.log("element number"+taskId+" does not exist");
+    }
+
+}
+
 addTask("test task 1", "2025-08-06", false);
 addTask("test task 2", "2025-08-03", true);
 
-allTasks.forEach((task) => task.taskInfo());
+function showAllTasksConsole() {
+    allTasks.forEach((task) => task.taskInfo());
+}
+window.showAllTasksConsole = showAllTasksConsole;
+window.addTask = addTask;
+window.deleteTask = deleteTask;
+
+
+
