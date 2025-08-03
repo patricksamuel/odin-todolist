2c5e8cc16e45928b14e3e27adbe37a70a3d64c63
Patrick Samuel <10101460+patricksamuel@users.noreply.github.com>
2025-08-03 20:10:29 +0200

    create intial class for task some construction function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
 import "./styles.css";
+import { format, isBefore, differenceInDays } from "date-fns";
+
+
 console.log("index.js test");
+
+
+const allTasks = [];
+class Task {
+    constructor(taskId,taskDesc, dueDate,doneStatus,createdDate, doneDate) {
+        this.taskId = taskId;
+        this.taskDesc = taskDesc;
+        this.dueDate= dueDate;
+        this.doneStatus = doneStatus;
+        this.createdDate = format(new Date(), "yyyy-MM-dd");
+        this.doneDate = doneDate;
+        }
+    taskInfo(){
+        if (this.doneStatus) {
+            console.log("You have to do: "+this.taskDesc+" by "+this.dueDate+" And you did it on "+this.doneDate);
+            }
+        else {
+            console.log("You have to do: "+this.taskDesc+" by "+this.dueDate+" not done yet");
+            }
+        }
+    }
+function addTask(taskDesc, dueDate, doneStatus) {
+    const taskId = crypto.randomUUID();
+    const createdDate = format(new Date(), "yyyy-MM-dd");
+    const doneDate = doneStatus ? createdDate : null;
+    const newTask = new Task(taskId,taskDesc, dueDate, doneStatus,createdDate, doneDate);
+    allTasks.push(newTask);
+}
+
+addTask("test task 1", "2025-08-06", false);
+addTask("test task 2", "2025-08-03", true);
+
+allTasks.forEach((task) => task.taskInfo());
