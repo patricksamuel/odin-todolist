98a18c6e5798142192dcf512e0a968b7dd8a751b
Patrick Samuel <10101460+patricksamuel@users.noreply.github.com>
2025-08-09 13:23:49 +0200

    local storage implemented. Some refactoring. Can now edit each line and also add toggle button. Ready for front end

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import "./styles.css";
 import { format, isBefore, differenceInDays } from "date-fns";
 
 
-console.log("index.js test");
+console.log("index.js test 3");
 
 
-const allTasks = [];
+//Task class definition
 class Task {
     constructor(taskId,taskDesc, dueDate,doneStatus,createdDate, doneDate) {
         this.taskId = taskId;
         this.taskDesc = taskDesc;
         this.dueDate= dueDate;
         this.doneStatus = doneStatus;
-        this.createdDate = format(new Date(), "yyyy-MM-dd");
+        this.createdDate = createdDate;
         this.doneDate = doneDate;
         }
     taskInfo(){
@@ -24,13 +24,67 @@ class Task {
             }
         }
     }
+
+
+function storageAvailable(type) {
+    let storage;
+    try {
+    storage = window[type];
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+    } catch (e) {
+    return (
+        e instanceof DOMException &&
+        e.name === "QuotaExceededError" &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+    );
+    }
+}
+// function to store task object locally
+function saveAllTasksLocal(allTasks) {
+    localStorage.setItem("allTasks", JSON.stringify(allTasks));
+}
+
+// reload local taks storage
+function loadAllTasks() {
+  const raw = JSON.parse(localStorage.getItem("allTasks")) || [];
+  return raw.map(task => new Task(task.taskId,task.taskDesc, task.dueDate, task.doneStatus,task.createdDate, task.doneDate))
+}
+
+
+let allTasks = [];
+
+ if (storageAvailable("localStorage")) {
+  allTasks = loadAllTasks();
+} 
+else {
+  saveAllTasksLocal(allTasks);
+} 
+
+/* allTasks = loadAllTasks();
+ */
+
+
+if (allTasks.length === 0) {
+  addTask("test task 1", "2025-08-06", false);
+  addTask("test task 2", "2025-08-03", true);
+}
+
+
+
 function editTask(taskId,taskDesc, dueDate, doneStatus) {
     const index = allTasks.findIndex((task) => task.taskId === taskId)
     const taskToEdit = allTasks[index]
     taskToEdit.taskDesc = taskDesc;
     taskToEdit.dueDate = dueDate;
     console.log("task edited");
+    saveAllTasksLocal(allTasks);
 }
+
 
 function addTask(taskDesc, dueDate, doneStatus) {
     const taskId = crypto.randomUUID();
@@ -39,13 +93,16 @@ function addTask(taskDesc, dueDate, doneStatus) {
     const newTask = new Task(taskId,taskDesc, dueDate, doneStatus,createdDate, doneDate);
     allTasks.push(newTask);
     console.log("task added");
+    saveAllTasksLocal(allTasks);
 }
 
 function deleteTask(taskId) {
     const index = allTasks.findIndex((task) => task.taskId === taskId)
     if (index !== -1){
         allTasks.splice(index,1);
+
         console.log("element number"+taskId+" is deleted");
+        saveAllTasksLocal(allTasks);
     }
     else {
         console.log("element number"+taskId+" does not exist");
@@ -53,9 +110,21 @@ function deleteTask(taskId) {
 
 }
 
-addTask("test task 1", "2025-08-06", false);
-addTask("test task 2", "2025-08-03", true);
+const editForm = document.createElement("Div") // create edit container
+// edit task input
+const EditTaskDescLabel = document.createElement("label");
+EditTaskDescLabel.textContent = "Task Description"
+const EditTaskDescInput = document.createElement("input");
 
+// Save Button to edit
+const saveEditButton = document.createElement("button");
+editForm.appendChild(EditTaskDescLabel);
+editForm.appendChild(EditTaskDescInput);
+editForm.appendChild(saveEditButton);
+saveEditButton.textContent = "Save";
+
+
+//show all task on console
 function showAllTasksConsole() {
     allTasks.forEach((task) => task.taskInfo());
 }
@@ -68,44 +137,53 @@ function showAllTasksDom(){
     mainContainer.textContent = '';
     allTasks.forEach(task => {
         const li = document.createElement("li");
+        // check done button
+        const doneButton = document.createElement("input");
+        doneButton.type = "checkbox";
+        doneButton.checked=task.doneStatus;
+        doneButton.addEventListener("click", (e) => {
+            task.doneStatus = doneButton.checked;
+            task.doneDate = doneButton.checked ? format(new Date(), "yyyy-MM-dd") : null;
+            saveAllTasksLocal(allTasks);
+            showAllTasksDom();
+            //li.innerHTML = "";
+            //li.textContent = `Task Id: ${task.taskId}. You have to do: ${task.taskDesc} by ${task.dueDate} And you did it on ${task.doneDate}`;
+            //if (task.doneStatus === true){
+            //    li.style.textDecoration = "line-through";
+            //}
+            //li.appendChild(deleteButton);
+            //li.appendChild(editButton);
+        });
+        // delete button
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "delete";
-        const editButton = document.createElement("button");
-        editButton.textContent = "edit";
-        
-
         deleteButton.addEventListener("click", (e) => {
             deleteTask(task.taskId);
+            saveAllTasksLocal(allTasks);
             mainContainer.removeChild(li)
         });
-        
+        //Edit button
+        const editButton = document.createElement("button");
+        editButton.textContent = "edit";
         editButton.addEventListener("click", (e) => {
             console.log("want to edit");
-            const editForm = document.createElement("Div")
-            const EditTaskDescLabel = document.createElement("label");
-            EditTaskDescLabel.textContent = "Task Description"
-            const EditTaskDescInput = document.createElement("input");
             EditTaskDescInput.value = task.taskDesc;
-            const saveEditButton = document.createElement("button");
-            editForm.appendChild(EditTaskDescLabel)
-            editForm.appendChild(EditTaskDescInput)
-            editForm.appendChild(saveEditButton)
-            saveEditButton.textContent = "Save";
+            
             li.appendChild(editForm);
             saveEditButton.addEventListener("click",(e)=>{
-
                 editTask(task.taskId,EditTaskDescInput.value, task.dueDate, task.doneStatus);
-                li.innerHTML = ""
-                li.textContent = `Task Id: ${task.taskId}. You have to do: ${task.taskDesc} by ${task.dueDate} And you did it on ${task.doneDate}`;
-                //li.removeChild(editForm);
-                li.appendChild(deleteButton);
-                li.appendChild(editButton);
+                saveAllTasksLocal(allTasks);
+                showAllTasksDom();
             })
 
             
         });
-
+        
         li.textContent = `Task Id: ${task.taskId}. You have to do: ${task.taskDesc} by ${task.dueDate} And you did it on ${task.doneDate}`;
+        if (task.doneStatus === true){
+            li.style.textDecoration = "line-through";
+        }
+        li.appendChild(doneButton);
         li.appendChild(deleteButton);
         li.appendChild(editButton);
         mainContainer.appendChild(li);
